8fba40a872d33cc7721ff635a542c17c0ca1be86
srojas <srojas@cloudspace.com.ar>
2021-04-27 12:47:55 -0300

    subir proyecto

---
+++ data/formulario.php
@@ -0,0 +1,19 @@
+﻿<?php 
+    $cuerpo= '';
+    $email='pedidos@tiendavirtualcoder.com';
+
+    $cuerpo .= "Pedido generado correctamente desde la web";
+    $cuerpo .= "\n";
+
+    $cabeceras = 'Reply-To:' .$email . "\r\n" .                     
+                 'MIME-Version: 1.0' . "\r\n".
+                 'Content-type: text/html; charset=utf-8' . "\r\n".
+                 'X-Mailer: PHP/' . phpversion();
+
+
+    //DIRECCIÓN
+    $enviarA = 'sec.rojas@gmail.com';
+    $asunto = 'Realizaron un pedido desde la web de la tienda';
+
+    $success = mail($enviarA,$asunto,$cuerpo,$cabeceras);
+?>
